9fd840f50333c45e50181d3ecb3eb3a4f8f667f3
tasha kijsamnong <tkijsamnong@yahoo.com>
2014-07-30 15:46:25 -0400

    updated with fixed border

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,23 +2,49 @@
 $(".home-button").click(function() {
     $('body').animate({
         scrollTop: $(".header").offset().top
-    }, 700);
+    }, 800);
 });
 
 $(".contact-button").click(function() {
     $('body').animate({
         scrollTop: $(".footer").offset().top
-    }, 700);
+    }, 800);
 });
 
 $(".bio-button").click(function() {
     $('body').animate({
         scrollTop: $(".container").offset().top
-    }, 700);
+    }, 800);
 });
 
 $(".projects-button").click(function() {
     $('body').animate({
         scrollTop: $(".project-container").offset().top
-    }, 700);
+    }, 800);
+});
+
+$(document).ready(function(){
+    $('.home-button').on('click', function(){
+        if($('.header').css('visibility') == 'hidden') {
+            $('.header').css('visibility', 'visible');
+        } else {
+            $('.header').css('visibility', 'hidden');
+        }
+    });
+
+    $('#close-nav').on('click', function(){
+        $('.nav-display').css('visibility', 'hidden');
+    });
+
+    $(".nav-display a").bind("click", function(e){
+        e.preventDefault();
+        var location = $(this).attr("href");     
+        $('html, body').animate({
+            scrollTop: $(location).offset().top
+        }, 1500);
+    });
+
+    $('.print').click(function(){
+        window.print();
+    });
 });
