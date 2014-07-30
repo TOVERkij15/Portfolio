a018cff685340276a2074058011d8dfc370932c7
tasha kijsamnong <tkijsamnong@yahoo.com>
2014-07-30 03:30:06 -0400

    updated with js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+
+$(".home-button").click(function() {
+    $('body').animate({
+        scrollTop: $(".header").offset().top
+    }, 700);
+});
+
+$(".contact-button").click(function() {
+    $('body').animate({
+        scrollTop: $(".footer").offset().top
+    }, 700);
+});
+
+$(".bio-button").click(function() {
+    $('body').animate({
+        scrollTop: $(".container").offset().top
+    }, 700);
+});
+
+$(".projects-button").click(function() {
+    $('body').animate({
+        scrollTop: $(".project-container").offset().top
+    }, 700);
+});
